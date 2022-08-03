7d5a42fc3eacaa6de8ebed6c66b85e83e63abc82
Paul Nizet <polonizet@gmail.com>
2022-08-04 00:43:00 +0200

    finish the clock, hell yeah

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,59 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(callback) {
-    // ... your code goes here
+    if (callback) {
+      this.intervalId = setInterval(() => {
+        callback();
+        this.currentTime++;
+      }, 1)
+    } else {
+      this.intervalId = setInterval(() => {
+        this.currentTime++;
+      }, 100)
+    }
   }
 
   getMinutes() {
-    // ... your code goes here
+    const numberOfMinutes = Math.floor(this.currentTime / (60))
+    return numberOfMinutes
   }
 
   getSeconds() {
-    // ... your code goes here
+    const numberOfSeconds = this.currentTime % 60
+    return numberOfSeconds
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let valueString = value.toString()
+
+    if (valueString.length === 1) {
+      valueString = "0" + valueString;
+    }
+    return valueString
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
+    this.intervalId = false;
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
+    document.getElementsByClassName('number').textContent = "0";
   }
 
+  //The split method should expect no arguments, and return a string where the time since the start is formatted as "mm:ss". Internally, the split method can make usage of previously declared methods such as getMinutes, getSeconds, and computeTwoDigitNumber.
+
   split() {
-    // ... your code goes here
+    const minutes = this.getMinutes()
+    const seconds = this.getSeconds()
+    const minutesTwoDigits = this.computeTwoDigitNumber(minutes)
+    const secondsTwoDigits = this.computeTwoDigitNumber(seconds)
+    return minutesTwoDigits + ":" + secondsTwoDigits
   }
 }
 
--- javascript/index.js
+++ javascript/index.js
@@ -3,6 +3,7 @@ const chronometer = new Chronometer();
 // get the buttons:
 const btnLeftElement = document.getElementById('btnLeft');
 const btnRightElement = document.getElementById('btnRight');
+const resetAction = document.getElementsByClassName('reset');
 
 // get the DOM elements that will serve us to display the time:
 const minDecElement = document.getElementById('minDec');
@@ -14,52 +15,93 @@ const milUniElement = document.getElementById('milUni');
 const splitsElement = document.getElementById('splits');
 
 function printTime() {
-  // ... your code goes here
+  printSeconds();
+  printMinutes();
 }
 
+
+
 function printMinutes() {
-  // ... your code goes here
+  let minutesUnits = chronometer.getMinutes()
+  minUniElement.textContent = Math.floor(minutesUnits % 10)
+  if (chronometer.currentTime >= 600) {
+    minDecElement.textContent = Math.floor((minutesUnits / 10) % 60)
+  }
 }
 
 function printSeconds() {
-  // ... your code goes here
+  let secondsUnits = chronometer.getSeconds()
+
+  secUniElement.textContent = Math.floor(secondsUnits % 10)
+
+  if (chronometer.currentTime >= 10) {
+    secDecElement.textContent = Math.floor((secondsUnits / 10) % 60)
+  }
 }
 
 // ==> BONUS
 function printMilliseconds() {
-  // ... your code goes here
+
 }
 
 function printSplit() {
-  // ... your code goes here
+
 }
 
 function clearSplits() {
-  // ... your code goes here
+
 }
 
 function setStopBtn() {
-  // ... your code goes here
+
 }
 
 function setSplitBtn() {
-  // ... your code goes here
+
 }
 
 function setStartBtn() {
-  // ... your code goes here
+
 }
 
 function setResetBtn() {
-  // ... your code goes here
+
 }
 
 // Start/Stop Button
 btnLeftElement.addEventListener('click', () => {
-  // ... your code goes here
+  if (!chronometer.intervalId) {
+    btnLeftElement.classList.remove('start')
+    btnLeftElement.classList.add('stop')
+    btnLeftElement.textContent = 'STOP'
+    btnLeftElement.classList.remove('reset')
+    btnRightElement.classList.add('split')
+    btnRightElement.textContent = 'SPLIT'
+    chronometer.start(printTime);
+  }
+  else {
+    btnLeftElement.classList.remove('stop')
+    btnLeftElement.classList.add('start')
+    btnLeftElement.textContent = 'START'
+    btnRightElement.classList.remove('split')
+    btnRightElement.classList.add('reset')
+    btnRightElement.textContent = 'RESET'
+    chronometer.stop();
+  }
 });
 
 // Reset/Split Button
 btnRightElement.addEventListener('click', () => {
-  // ... your code goes here
+  if (btnRightElement.classList.contains('split')) {
+    splitsElement.innerHTML += `<li>00:${chronometer.split()}</li>`
+    console.log(chronometer.currentTime)
+  }
+  else {
+    console.log('reset')
+    chronometer.reset();
+    minUniElement.textContent = '0'
+    minDecElement.textContent = '0'
+    secUniElement.textContent = '0'
+    secDecElement.textContent = '0'
+  }
 });
